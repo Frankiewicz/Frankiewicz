4febef13eaa451b30b13c740e47c13ef91354fb0
Frankiewicz <wfrankiewicz1999@wp.pl>
2018-01-24 10:27:14 +0100

    assas

---
+++ TEB/3_typyDanych.js
@@ -0,0 +1,11 @@
+var dlugosc = prompt("Podaj długość boku:");
+var wyniki = dlugosc * dlugosc;
+document.write("Długość boku wynosi " + dlugosc + "cm<br>" );
+document.write("Pole kwadratu wynosi " + wyniki + "cm<sup>2</sup>");
+
+
+
+
+//zad.dom.
+
+//Napisz program obliczający pole trójkąta
